39ddf0699ce1541a4ef8dcbf1c7d6e89654deab7
Augusto0192 <31197851+Augusto0192@users.noreply.github.com>
2025-08-05 14:23:04 -0300

    Create hamerHand.lua

---
+++ player/hamerHand.lua
@@ -0,0 +1,15 @@
+local player = {}
+
+player.name = "Mão de Martélo"
+player.description = "Mestre das Forjas e do fogo"
+
+player.maxHealth = 15
+player.health = 15
+player.attack = 3
+player.defense = 3
+player.speed = 2
+
+player.potions = 3
+player.especial = 1
+
+return player
